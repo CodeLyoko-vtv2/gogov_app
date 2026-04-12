a0c622ed9757fb13fcf4897f3d3179879f7e9e6f
Houseless44 <hieubuilethe@gmail.com>
2026-04-12 16:29:52 +0700

    feat: code man hinh ChiDuong-HienTruong

---
+++ app/(rescue)/HienTruong.tsx
@@ -0,0 +1,6 @@
+// app/(rescue)/HienTruongScreen.tsx
+import HienTruongScreen from "../../src/screens/RescueTeam/Missions/HienTruongScreen";
+
+export default function HienTruong() {
+  return <HienTruongScreen />;
+}
